b7d7dde6bf374f8e59c0ada9ff232cf0d5edbafa
chris_jones <chrisjns679@gmail.com>
2020-03-26 13:01:11 -0400

    Add project files.

---
+++ Group_5_Hospital_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Group_5_Hospital_Project.Startup))]
+namespace Group_5_Hospital_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
