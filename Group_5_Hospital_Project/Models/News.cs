9534a0d5d8f7faece25003b295ca3b6bd62ddb25
chris_jones <chrisjns679@gmail.com>
2020-04-01 13:33:20 -0400

    Fixed namespace errors in models News and find_doctor, and switched project to bootstrap 4, with minor nav fix

--- Group_5_Hospital_Project/Models/News.cs
+++ Group_5_Hospital_Project/Models/News.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Group_5_Hospital_Project.Data;
 
 namespace Group_5_Hospital_Project.Models
 {
