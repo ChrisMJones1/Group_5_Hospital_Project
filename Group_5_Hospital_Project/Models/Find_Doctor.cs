cf1ac157167b9689b19475e8ad54ed88463a5d09
krinapatel440@gmail.com <krinapatel440@gmail.com>
2020-04-01 00:32:27 -0400

    krina's commit for models

---
+++ Group_5_Hospital_Project/Models/Find_Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group_5_Hospital_Project.Models
+{
+    public class Find_Doctor
+    {
+        [Key]
+        public int doctor_id { get; set; }
+        public string doctor_name { get; set; }
+        public string doctor_email { get; set; }
+        public int doctor_phone { get; set; }
+        public string doctor_speciality { get; set; }
+        public string doctor_availabilty { get; set; }
+        public string doctor_experience { get; set; }
+    }
+}
---
+++ Group_5_Hospital_Project/Models/News.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group_5_Hospital_Project.Models
+{
+    public class News
+    {
+        [Key]
+        public int news_id { get; set; }
+        public string news_title { get; set; }
+        public DateTime newsposted_date { get; set; }
+        public DateTime newsupdated_date { get; set; }
+        public string news_information{ get; set; }
+       
+    }
+
+}
