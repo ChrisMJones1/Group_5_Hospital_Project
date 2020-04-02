d54d099ce1b4a2968ddfd218ed163d98227ae591
TimothyZhang <TimothyZhang@foxmail.com>
2020-04-02 08:16:15 +0800

    Tim: models (volunteer and appointment feature)

---
+++ Group_5_Hospital_Project/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group_5_Hospital_Project.Models
+{
+    public class Appointment
+    {
+        public int Appointment_ID { get; set; }
+        public DateTime Appointment_start_time { get; set; }
+        public DateTime Appointment_end_time { get; set; }
+        // many2many
+        public virtual ICollection<ApplicationUser> User { get; set; }
+    }
+}
---
+++ Group_5_Hospital_Project/Models/Volunteer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group_5_Hospital_Project.Models
+{
+    public class Volunteer
+    {
+        [Key]
+        public int Volunteer_ID { get; set; }
+        public string Volunteer_name { get; set; }
+        public string Volunteer_description { get; set; }
+        public DateTime Volunteer_start_time { get; set; }
+        public int Volunteer_maximum_headcount { get; set; }
+        public int Volunteer_applied_headcount { get; set; }
+
+        // many2many
+        public virtual ICollection<ApplicationUser> User { get; set; }
+    }
+}
