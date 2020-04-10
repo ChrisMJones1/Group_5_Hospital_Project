5f0284a6d977acc404cea97994eed4406523c35b
TimothyZhang <TimothyZhang@foxmail.com>
2020-04-10 12:58:55 +0800

    Tim: volunteer and appointment CRUD only, with no logic

--- Group_5_Hospital_Project/Migrations/202004100428202_init.Designer.cs
+++ Group_5_Hospital_Project/Migrations/202004100428202_init.Designer.cs
@@ -13,7 +13,7 @@ namespace Group_5_Hospital_Project.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "202004091309174_init"; }
+            get { return "202004100428202_init"; }
         }
         
         string IMigrationMetadata.Source
