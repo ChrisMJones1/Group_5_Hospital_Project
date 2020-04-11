2e3555c629321e99e2883c721476d04254e7c51e
Shanakay <shanakayhall@gmail.com>
2020-04-11 10:01:28 -0400

    edited patient model

--- Group_5_Hospital_Project/Models/Patient.cs
+++ Group_5_Hospital_Project/Models/Patient.cs
@@ -42,7 +42,7 @@ namespace Group_5_Hospital_Project.Models
         //Represents the "many" in (one Patient to many Send Best Wishes Forms)
         // public ICollection<Wishes> Wishes { get; set; }
 
-        //Represents the many in (one category to many furniture products)        
+        //Represents the many in (one Patient to many Send Best Wishes Forms)        
         public int WishesID { get; set; }
         [ForeignKey("WishesID")]
         public virtual Wishes Wishes { get; set; }
