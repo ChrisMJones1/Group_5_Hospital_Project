ca6ab1e8c86c8d00c06bd9815dd20d4f852b0596
Shanakay <shanakayhall@gmail.com>
2020-04-11 09:52:18 -0400

    updated wishes viewmodel

--- Group_5_Hospital_Project/Models/VIewModels/UpdatePatient.cs
+++ Group_5_Hospital_Project/Models/VIewModels/UpdatePatient.cs
@@ -8,8 +8,6 @@ namespace Group_5_Hospital_Project.Models.ViewModels
     public class UpdatePatient
     {
 
-
-
         //we need the patient info and a list of forms
 
         public Patient Patient { get; set; }
--- Group_5_Hospital_Project/Models/VIewModels/ViewWishes.cs
+++ Group_5_Hospital_Project/Models/VIewModels/ViewWishes.cs
@@ -8,9 +8,6 @@ namespace Group_5_Hospital_Project.Models.ViewModels
     public class ViewWishes
     {
 
-        //show one category
-
-        //show list of furniture that belongs to the category 
 
 
 
